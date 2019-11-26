c15b7a5b6b0af170958ca1b367d3848d79d89aeb
Wahyu2257 <48467871+Wahyu2257@users.noreply.github.com>
2019-11-26 07:23:09 -0800

    Add files via upload

---
+++ Tugas Delegate & Event/Windows Form Calculator/Calculator/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Kalkulator : Form
+    {
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        void FrmCalculator_Hitung(int index, int a, int b)
+        {
+            var operasi = new Hitung();
+
+            if(index == 0)
+            {
+                lsthasil.Items.Add(string.Format("Hasil Penjumlahan : {0} + {1} = {2}", a, b, operasi.Jumlahkan(a, b)));
+            }
+            else if(index == 1)
+            {
+                lsthasil.Items.Add(string.Format("Hasil Pengurangan : {0} - {1} = {2}", a, b, operasi.Kurangkan(a, b)));
+            }
+            else if (index == 2)
+            {
+                lsthasil.Items.Add(string.Format("Hasil Perkalian : {0} * {1} = {2}", a, b, operasi.Kalikan(a, b)));
+            }
+            else
+            {
+                lsthasil.Items.Add(string.Format("Hasil Pembagian : {0} / {1} = {2}", a, b, operasi.Bagikan(a, b)));
+            }
+
+        }
+
+        private void BtnHitung_Click(object sender, EventArgs e)
+        {
+            Form2 myForm = new Form2();
+
+            myForm.SyehKalkulasi += FrmCalculator_Hitung;
+            myForm.ShowDialog();
+        }
+
+        private void lsthasil_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Tugas Delegate & Event/Windows Form Calculator/Calculator/Hitung.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Hitung
+    {
+        public int Jumlahkan(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Kurangkan(int a, int b)
+        {
+            return a - b;
+        }
+        public int Kalikan(int a, int b)
+        {
+            return a * b;
+        }
+        public int Bagikan(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
